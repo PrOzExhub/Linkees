be0f2fed34e128780987929884cd62fac854e570
PrOzEx <67564876+PrOzExhub@users.noreply.github.com>
2022-02-08 23:12:14 +0100

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -8,53 +8,49 @@ import telegram from '../assets/telegram.png'
 import twitter from '../assets/twitter.png'
 
 const items = [{
-    "title": "Website",
-    "subtitle": "Look at my work!",
-    "image": website,
-    "link": "https://sagniksahoo.me" //your personal website or portfolio  link
-},
-{
-    "title": "GitHub",
-    "subtitle": "@devgossips | 🏠 of my open-source projects",
-    "image": github,
-    "link": "https://github.com/devgossips" //Github Profile link
-},
-{
-    "title": "Instagram",
-    "subtitle": "@heysagnik | Hub of my awesome 😎 pics",
-    "image": instagram,
-    "link": "https://instagram.com/heysagnik" //instagram profile link 
-},
-{
-    "title": "Twitter",
-    "subtitle": "@heysagnik | Don't forget to follow me 😉",
-    "image": twitter,
-    "link": "https://twitter.com/heysagnik"// twitter profile link 
-},
-{
-    "title": "Apps",
-    "subtitle": "Hub of my awesome 🔥 Apps",
-    "image": appstore,
-    "link": "#"//
-},
-{
-    "title": "YouTube",
-    "subtitle": "@Sagnik Sahoo | Official channel of mine",
-    "image": youtube,
-    "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"//youtube channel link 
-},
+        "title": "Snapchat",
+        "subtitle": "safadi.saif",
+        "image": snapchat,
+        "link": "https://story.snapchat.com/@safadi.saif?sender_web_id=484bab4d-a5a1-41e5-b2e4-783acd6dcc95&device_type=desktop&is_copy_url=true" //your personal website or portfolio  link
+    },
+    {
+        "title": "Facebook",
+        "subtitle": "@saif.safadi1 |",
+        "image": facebook,
+        "link": "https://www.facebook.com/saif.safadi1" //Github Profile link
+    },
+    {
+        "title": "Instagram",
+        "subtitle": "Follow me on --> @safadisaif ",
+        "image": instagram,
+        "link": "https://www.instagram.com/safadisaif/" //instagram profile link 
+    },
+    {
+        "title": "Twitter",
+        "subtitle": "Follow me on --> @saif_safadi  ",
+        "image": twitter,
+        "link": "https://twitter.com/saif_safadi" // twitter profile link 
+    },
 
-{
-    "title": "Dribbble",
-    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
-    "image": dribbble,
-    "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
-},
-{
-    "title": "Telegram",
-    "subtitle": "@virtuonic | Chat with me instantly, Don't hesitate! ",
-    "image": telegram,
-    "link": "https://telegram.me/heysagnik" //Telegram Pofile 
-}]
+    {
+        "title": "YouTube",
+        "subtitle": "@Saif Safadi | My Official channel ",
+        "image": youtube,
+        "link": "https://www.youtube.com/channel/UChxbIWYhBlpneSTa5bFfy0A/about" //youtube channel link 
+    },
+
+    {
+        "title": "Anghami",
+        "subtitle": "@سيف الصفدي ",
+        "image": anghami,
+        "link": "https://play.anghami.com/artist/11914366" // Dribbble profile link 
+    },
+    {
+        "title": "Spotify",
+        "subtitle": "@Saif Safadi ",
+        "image": spotify,
+        "link": "https://open.spotify.com/user/31zqrkmcywij77aga6wz54ydlr7u?si=z1T4W1VDRbWHdtqXtN5_xw&nd=1" //Telegram Pofile 
+    }
+]
 
 export default items
